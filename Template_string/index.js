53bce4bb468b892027aff3672246e0991da83605
agostina-s <agosaucedo17@gmail.com>
2022-04-01 13:32:09 -0300

    Template_String

---
+++ Template_string/index.js
@@ -0,0 +1,10 @@
+function mostrar (){
+    const nombre_apellido = 'Saucedo Daiana'
+    console.log('Nombre y Apellido: ' + nombre_apellido)
+}
+function mostrar2 (){
+    const nombre_apellido = 'Saucedo Daiana'
+    console.log(`
+    Nombre y Apellido: ${nombre_apellido}
+    `)
+}
